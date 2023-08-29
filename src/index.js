8272a6cd4bee9abc9f0bc47a5b3ac5268a1b1a51
Pankaj Sharma <pankajsharma376779@gamil.com>
2023-08-29 18:03:36 +0530

    Feat: Added basic user model ```  npx sequelize model:generate --name User --attributes  name:string,password:string  ```

--- src/index.js
+++ src/index.js
@@ -5,7 +5,13 @@ const apiRoutes = require('./routes');
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+
 app.use('/api', apiRoutes);
+
+
 
 app.listen(ServerConfig.PORT, () => {
     console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
