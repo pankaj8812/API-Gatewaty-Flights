54e2e599701c02c123a9b90c0165113f120af77d
Pankaj Sharma <pankajsharma376779@gamil.com>
2023-09-07 14:58:39 +0530

    FEAT: Added Authenication for Booking flight

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const rateLimit = require('express-rate-limit');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const { ServerConfig } = require('./config');
+const { AuthRequestMiddlewares } = require('../src/middlewares')
 const apiRoutes = require('./routes');
 
 const app = express();
@@ -12,25 +13,31 @@ const limiter = rateLimit({
 	max: 10, // Limit each IP to 10 requests per `window` (here, per 2 minutes)
 });
 
+       
+app.use("/flightsService", // if the user gives us a request to /flightsService
+    [AuthRequestMiddlewares.checkAuth, AuthRequestMiddlewares.isAdmin],
+    createProxyMiddleware({
+        target: ServerConfig.FLIGHT_SERVICE, //  We will redirect the request to the Flight Service' IP address -> `http://localhost:3000`
+        changeOrigin: true,
+        pathRewrite: {'^/flightsService' : '/'} 
+    })
+);
+            
+app.use(
+    "/bookingService",
+    [AuthRequestMiddlewares.checkAuth],
+    createProxyMiddleware({
+        target: ServerConfig.BOOKING_SERVICE,
+        changeOrigin: true,
+        pathRewrite: {'^/bookingsService': '/'}
+    })
+);
+
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(limiter);
-
-
-app.use('/flightsService', createProxyMiddleware({ 
-    target: ServerConfig.FLIGHT_SERVICE,
-    changeOrigin: true, 
-    pathRewrite: {'^/flightsService' : '/'} 
-}));
-
-app.use('/bookingsService', createProxyMiddleware({ 
-    target: ServerConfig.BOOKING_SERVICE,
-    changeOrigin: true, 
-    pathRewrite: {'^/bookingsService': '/'}
-}));
-
+    
 app.use('/api', apiRoutes);
-
 
 
 app.listen(ServerConfig.PORT, () => {
